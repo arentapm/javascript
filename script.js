5e92f19fd61222224aacbaef1caa284c5c666f74
arentapm <arenta12345@gmail.com>
2024-04-19 20:52:47 +0700

    tugas javascript

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
+    var warna = ['red', 'yellow', 'blue', 'green', 'orange']; // Daftar warna 
     $("#ubahWarna").click(function(){
-        $("#teks").css("color", "red");
+        var randomIndex = Math.floor(Math.random() * warna.length); // Pilih warna secara acak
+        var warnaPilihan = warna[randomIndex];
+        $("#teks").css("color", warnaPilihan);
     });
 });
